b24b29f5df5b57bfbd2ad260c15612c79c46b748
Hannah Roat <nornalbion@gmail.com>
2013-06-02 13:48:38 +0100

    Huge slew of changes. Bump to v0.0.2
    	* Added basic sanity checker
     	* Added a couple of tests.
     	* Numerous fixes

--- test/ebnf.js
+++ test/ebnf.js
@@ -2,33 +2,42 @@ var should = require('should');
 var fs = require('fs');
 
 var parser = require('../lib/parser')
-   ,generator = require('../lib/ebnf-generator');
+   ,generator = require('../lib/ebnf-generator')
+   ,SanityChecker = require('../lib/sanitychecker.js');
 
-var ebnf = '';
-beforeEach(function(done) {
-	fs.readFile('ebnf.ebnf', {encoding: 'utf8'}, function(err, data) {
-		if(err) throw err;
-		ebnf = data.toString();
-		done();
+describe('Parser', function() {
+	it('should correctly parse simple grammars');
+	it('should correctly parse complex grammars');
+});
+
+describe('Sanity checker', function() {
+	it('should find unused identifiers', function() {
+		var ebnf = "test = 'hi';";
+		var ast = parser.parse(ebnf);
+		var problems = new SanityChecker(ast).check();
+		
+		problems.length.should.equal(1);
+		problems[0].type.should.equal("unused identifier");
+		problems[0].line.should.equal(1);
+		problems[0].column.should.equal(1);
+		problems[0].data.rule.identifier.name.should.equal("test");
 	});
-});
-
-describe('EBNF syntax check', function() {
-	it('should correctly parse the EBNF grammar');
-});
-
-describe('EBNF sanity check', function() {
-	it('should check for unused identifiers');
-	it('should check for duplicate productions');
+	it('should find undeclared identifiers', function() {
+		var ebnf = "test = undeclared;";
+		var ast = parser.parse(ebnf);
+		var problems = new SanityChecker(ast).check();
+		
+		problems.length.should.equal(2);
+		problems[0].type.should.equal("unused identifier");
+		problems[1].type.should.equal("undeclared identifier");
+		problems[1].line.should.equal(1);
+		problems[1].column.should.equal(8);
+		problems[1].data.identifier.name.should.equal("undeclared");
+	});
+	it('should find duplicate rule names');
 
 });
 
 describe('EBNF code generator', function() {
-	it('should output valid EBNF', function() {
-		var ast = parser.parse(ebnf);
-		ast.length.should.equal(10);
-		var newbnf = generator.generate(ast);
-		ast = parser.parse(ebnf);
-		ast.length.should.equal(10);
-	});
+	it('should output valid EBNF');
 });
